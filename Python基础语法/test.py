74aef154dec687e4ceffb7670b82c96b157a33bc
tuke <tuke@corp.netease.com>
2019-03-16 22:58:57 +0800

    Python基础语法

---
+++ Python基础语法/test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 文件名：test.py
+
+# 条件语句
+if True:
+    print("Answer")
+    print("True")
+else:
+    print("Answer")
+
+# 多行语句
+total = "item_one" + \
+        " " + "item_two"
+print(total)
+
+# Python 引号
+word = 'word'
+sentence = "这是一个句子"
+paragrath = """这是一个段落
+包含了多个段落
+"""
+print(word); print(sentence); print(paragrath);
+
+#  注释
+name = "Madisetti"; # 这是一个注释
+'''
+这是多行注释，使用单引号
+这是多行注释，使用单引号
+'''
+
+"""
+这是多好注释，使用双引号
+这厮多行注释，使用双引号
+"""
+
+# 等待用户输入
+input("按下 enter 键退出，其他任意键显示... \n")
+
+# 输出
+x = "a"
+y = "b"
+
+print("默认换行输出")
+print(x)
+print(y)
+
+print("不换行输出")
+print(x,y)
+
+
+
+
+
